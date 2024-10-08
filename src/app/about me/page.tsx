f701bf7c9076d60ed286ff23fb17a12761385141
Ryan <ryan.lim.2024@computing.smu.edu.sg>
2024-10-08 11:38:39 +0800

    init app

--- src/app/about me/page.tsx
+++ src/app/about me/page.tsx
@@ -1,8 +1,30 @@
 //import Image from "next/image";
 
-export default function Quiz() {
-    return (
-      <div>Hello World this is the about me page</div>
-    );
-  }
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function AboutMe() {
+  return (
+    <div>
+        Hello world this is the about me route
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    Hello I am ryan
+                </CardTitle>
+                <CardDescription>
+                    I like to code lots
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="flex flex-row gap-2">
+                    <p className="font-bold">Name:</p>
+                    Nicholas
+                </div>
+                <div className="flex flex-row gap-2"><p className="font-bold">Major:</p>Information Systems</div>
+                <div className="flex flex-row gap-2"><p className="font-bold">Hobbies:</p>I like to code</div>
+            </CardContent>
+        </Card>
+    </div>
+  );
+}
   
